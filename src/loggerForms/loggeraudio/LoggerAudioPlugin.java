cef0c64c3909fa3fae47cb4258fba16c6465a28a
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2026-07-15 11:17:15 +0100

    Make voices plugin
    Move logger voices to plugin since it's not suitable for general inclusion

---
+++ src/loggerForms/loggeraudio/LoggerAudioPlugin.java
@@ -0,0 +1,112 @@
+package loggerForms.loggeraudio;
+
+import PamController.PamguardVersionInfo;
+import PamModel.PamDependency;
+import PamModel.PamPluginInterface;
+
+public class LoggerAudioPlugin implements PamPluginInterface {
+
+	private String jarFile;
+
+	@Override
+	public String getDefaultName() {
+		return "Logger Voices";
+	}
+
+	@Override
+	public String getHelpSetName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setJarFile(String jarFile) {
+		this.jarFile = jarFile;
+	}
+
+	@Override
+	public String getJarFile() {
+		return jarFile;
+	}
+
+	@Override
+	public String getDeveloperName() {
+		return "Doug Gillespie";
+	}
+
+	@Override
+	public String getContactEmail() {
+		return "pamguard@pamguard.org";
+	}
+
+	@Override
+	public String getVersion() {
+		return "0.0";
+	}
+
+	@Override
+	public String getPamVerDevelopedOn() {
+		return "2.02.19";
+	}
+
+	@Override
+	public String getPamVerTestedOn() {
+		return "2.02.19";
+	}
+
+	@Override
+	public String getAboutText() {
+		return "Capture audio from oververs using the SCANS mobile app";
+	}
+
+	@Override
+	public String getClassName() {
+		return LoggerAudioControl.class.getName();
+	}
+
+	@Override
+	public String getDescription() {
+		return "Capture audio from oververs using the SCANS mobile app";
+	}
+
+	@Override
+	public String getMenuGroup() {
+		return "Visual Methods";
+	}
+
+	@Override
+	public String getToolTip() {
+		return getDescription();
+	}
+
+	@Override
+	public PamDependency getDependency() {
+		return null;
+	}
+
+	@Override
+	public int getMinNumber() {
+		return 0;
+	}
+
+	@Override
+	public int getMaxNumber() {
+		return 1;
+	}
+
+	@Override
+	public int getNInstances() {
+		return 0;
+	}
+
+	@Override
+	public boolean isItHidden() {
+		return false;
+	}
+
+	@Override
+	public int allowedModes() {
+		return 0;
+	}
+
+}
